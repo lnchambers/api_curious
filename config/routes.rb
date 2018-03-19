e1a9d0ef192cabfd30cc6bb9e340819c738f78c0
Luke Chambers <lukenatchambers@gmail.com>
2018-03-19 15:03:39 -0600

    Work on omniauth

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   root "welcome#index"
 
-  resources :sessions, only: [:create, :new]
+  get "/login", :to => 'sessions#new'
+  get '/auth/eve_online_sso/callback', :to => 'sessions#create'
+  get '/auth/failure', :to => 'sessions#create'
 end
