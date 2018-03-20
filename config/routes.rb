5b71a8bc1cb83f9693e8fea7bdb20caf566affa6
Luke Chambers <lukenatchambers@gmail.com>
2018-03-20 15:38:42 -0600

    Omniauth is functional

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   root "welcome#index"
 
   get "/login", :to => 'sessions#new'
-  get '/auth/eve/callback', :to => 'sessions#create'
+  get '/auth/:provider/callback', :to => 'sessions#create'
   get '/auth/failure', :to => 'sessions#create'
 end
