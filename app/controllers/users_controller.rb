103295c746166bb7d7295e8888ddd020e0295161
Luke Chambers <lukenatchambers@gmail.com>
2018-03-22 19:05:09 -0600

    Get Chuck Norris API

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,5 +5,9 @@ class UsersController < ApplicationController
     @image = parsed[:px256x256]
     response = Faraday.get("https://esi.tech.ccp.is/latest/characters/#{current_user.characters.first.id}/wallet/?token=#{current_user.characters.first.token}")
     parsed = JSON.parse(response.body, symbolize_names: true)
+    @wallet = parsed
+    response = Faraday.get("https://api.chucknorris.io/jokes/random")
+    parsed = JSON.parse(response.body)
+    @joke = parsed["value"]
   end
 end
